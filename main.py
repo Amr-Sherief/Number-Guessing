9bdaff94cc241ba2533dcae918cd9833c8abfe11
Amr <sherie1165@gmail.com>
2020-11-06 17:49:22 +0200

    some shit

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ try:
 
     guessing_numbers = range(first, second)
     number = random.choice(guessing_numbers)
+
+    try_1 = input()
+
+    if try_1 == number:
+        print("Congratulations")
 except ValueError:
     print("Insert digits ONLY")
