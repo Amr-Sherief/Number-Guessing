a5c39f95bbe347dd8c5b701ef4d684b06aa3cf83
Amr <sherie1165@gmail.com>
2020-11-12 17:26:20 +0200

    installed numpy installed colors added colors to the first two texts and fixed some writing

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import random
-
+from colors import *
 small_numbers = [1, 2, 3]
 
 try:
     
-    print("Enter the number of tries:")
+    print(colors.red("Enter the number of tries:"))
 
     number_of_retries = input()
     number_of_retries_list = []
     for i in range(1, int(number_of_retries)):
         number_of_retries_list.insert(0, '0')
 
+    print(red("Enter the range of Guessing:"))
 
-    print("Enter the range of Guessing:")
-
-    insert = input()                                  #the insert of the player
+    insert = input()                                  # the insert of the player
     insert = insert.split(" ")
 
-    first = int(insert[0])                            #the first number of the guessing range
-    second = int(insert[1])                           #the second number of the guessing range
+    first = int(insert[0])                            # the first number of the guessing range
+    second = int(insert[1])                           # the second number of the guessing range
 
-    guessing_numbers = range(first, second)           #the guessing range
-    number = random.choice(guessing_numbers)          #the random number that the player should guess
-
-                                                      #the player's guess of the number
+    guessing_numbers = range(first, second)           # the guessing range
+    number = random.choice(guessing_numbers)          # the random number that the player should guess
+    # the player's guess of the number
     for i in number_of_retries_list:
         small_guess = random.choice(small_numbers)
         small_guess_2 = random.choice(small_numbers)
@@ -44,36 +42,41 @@ try:
             print("Number is " + str(number))
             print("Stupid")
             break
-        elif 1 < second - first < 6 and ((second - 1) - (first + 1)) != 0 and number in range((first+1),(second-1)):  # Acts on numbers of difference between 1 and 6
+        elif 1 < second - first < 6 and ((second - 1) - (first + 1)) != 0 and number in range((first+1), (second-1)):
+            # Acts on numbers of difference between 1 and 6
             print("Number is between " + str(first + 1) + " " + str(second - 1))  # Gives the player a hint
             first = first + 1
-            second = second -1
+            second = second - 1
             continue
-        elif second - first == 6 and number in range((first+2),(second-2)):  # Acts if the difference between the numbers is 6
+        elif second - first == 6 and number in range((first+2), (second-2)):
+            # Acts if the difference between the numbers is 6
             print("Number is between " + str(first + 2) + " " + str(second - 2))
             first = first + 2
             second = second - 2
             continue
-        elif 7 == second - first and number in range((first+1),(second-1)):                          # Acts if the difference between the numbers is 7
+        elif 7 == second - first and number in range((first+1), (second-1)):
+            # Acts if the difference between the numbers is 7
             print("Number is between " + str(first + 1) + " " + str(second - 2))
             first = first + 1
             second = second - 2
             continue
-        elif 7 < second - first < 10 and number in range((first+small_guess),(second-small_guess_2)): #Acts if the difference between the two numbers is between 7 and 10
+        elif 7 < second - first < 10 and number in range((first+small_guess), (second-small_guess_2)):
+            # Acts if the difference between the two numbers is between 7 and 10
             print("Number is between " + str(first + small_guess) + " " + str(second - small_guess_2))
             first = first + small_guess
             second = second - small_guess_2
             continue
-        elif 10 == second - first and number in range((first + 3),(second - 3)):             #Acts if the difference between the two numbers is 10
+        elif 10 == second - first and number in range((first + 3), (second - 3)):
+            # Acts if the difference between the two numbers is 10
             print("Number is between " + str(first + 3) + " " + str(second - 3))
             first = first + 3
             second = second - 3
             continue
-    else:                                                           #Acts if the loop ends
+    else:                                                           # Acts if the loop ends
         print("\nThe number is " + str(number))
 
 
 except ValueError:
     print("Insert digits ONLY")
 
-#todo: add colors to the messages of the program
+# todo: add colors to the messages of the program
